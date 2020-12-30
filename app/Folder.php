aa75ad95ab87522442cd872507b816802c54fad9
EC2 Default User <ec2-user@ip-172-31-14-170.us-east-2.compute.internal>
2020-12-30 15:29:01 +0000

    task完成

--- app/Folder.php
+++ app/Folder.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    public function tasks(){
+        return $this->hasMany('App\Task','folder_id','id');//第一 名前空間含むモデル名　第二　外部キー（親側）　第三　内部キー（子側）
+    }
 }
